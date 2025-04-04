ff3924a9c658c72d71f2a79a83df0e0ee224e515
Dieter Bergmann <dieterbergmann@arcor.de>
2025-04-04 15:16:11 +0200

    Initial Commit

---
+++ Git Project 2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Git Project 2025
+//
+//  Created by Dieter Bergmann on 04.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Git Project 2025/Git_Project_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Project_2025App.swift
+//  Git Project 2025
+//
+//  Created by Dieter Bergmann on 04.04.25.
+//
+
+import SwiftUI
+
+@main
+struct Git_Project_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
